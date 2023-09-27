55c60d4908568566d1436555041fe1d91171e23e
Pedro Lopes <pl.pedrogplopes@gmail.com>
2023-09-27 17:26:27 -0300

    Update Store.js

--- src/Components/Store/Store.js
+++ src/Components/Store/Store.js
@@ -3,10 +3,13 @@ import { create } from 'zustand';
 export const useStore = create((set) => ({
     tamanhoSelecionado: null,
     saborSelecionado: null,
+    ingredientesSelecionados: [],
     saboresData: [],
+    ingredientesData: [],
 
     setSelectedTamanho: (tamanho) => set({ tamanhoSelecionado: tamanho }),
     setSelectedSabor: (sabor) => set({ saborSelecionado: sabor }),
+    setIngredientesSelecionados: (ingredientes) => set({ ingredientesSelecionados: ingredientes }), 
     calcularPrecoTotal: () => {
         const state = useStore.getState();
         const tamanho = state.tamanhoSelecionado;
@@ -21,10 +24,31 @@ export const useStore = create((set) => ({
                 return precoTamanho;
             }
         }
+        return 0;
+    },
+
+    calcularPrecoIngredientes: () => {
+        const state = useStore.getState();
+        const tamanho = state.tamanhoSelecionado;
+        const ingredientesSelecionados = state.ingredientesSelecionados;
+        const ingredientesData = state.ingredientesData;
+
+        if (tamanho && ingredientesSelecionados.length > 0) {
+            const precoIngredientes = ingredientesSelecionados.reduce((total, ingrediente) => {
+                const ingredienteEncontrado = ingredientesData.find((item) => item.id === ingrediente.id);
+                if (ingredienteEncontrado && ingredienteEncontrado.preco) {
+                    return total + ingredienteEncontrado.preco;
+                }
+                return total;
+            }, 0);
+
+            return precoIngredientes;
+        }
 
         return 0;
     },
-    reset: () => set({ tamanhoSelecionado: null, saborSelecionado: null }),
+
+    reset: () => set({ tamanhoSelecionado: null, saborSelecionado: null, ingredientesSelecionados: [] }),
 }));
 
 export default useStore;
